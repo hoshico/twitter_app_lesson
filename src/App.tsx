df14b2a8962acc7fc2706bfe91e19d91fa09773f
hoshico <melancholinista.yasu@gmail.com>
2022-02-02 07:40:09 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
 
-function App() {
+const App:React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  
+  // userが変わったタイミングで呼び出される(userの監視をする)
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(login({
+          uid: authUser.uid,
+          photoUrl: authUser.photoURL,
+          displayName: authUser.displayName,
+        }))
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    }
+  }, [dispatch]);
+
   return (
     <div className="App">
      
