a0b273e211d16304b56ccea322ce594502a34732
hoshico <melancholinista.yasu@gmail.com>
2022-02-15 08:15:52 +0900

    add Tweetinput.tsx

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { auth } from '../firebase'
+import Tweetinput from './Tweetinput'
 
 const Feed = () => {
   return (
     <div>
       Feed
+      <Tweetinput />
       <button onClick={() => auth.signOut()}>LogOut</button>
     </div>
   )
